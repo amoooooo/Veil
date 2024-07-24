5896d9b2330aef3ac267f2914590024cce723e34
shBLOCK <shblock3@gmail.com>
2024-07-24 19:55:43 +0800

    Fixed reusing shader sources with causing confliction of default definitions.

--- common/src/main/java/foundry/veil/impl/client/render/shader/CachedShaderCompiler.java
+++ common/src/main/java/foundry/veil/impl/client/render/shader/CachedShaderCompiler.java
@@ -4,7 +4,6 @@ import foundry.veil.api.client.render.shader.CompiledShader;
 import foundry.veil.api.client.render.shader.ShaderCompiler;
 import foundry.veil.api.client.render.shader.ShaderException;
 import foundry.veil.api.client.render.shader.program.ProgramDefinition;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceProvider;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
@@ -29,19 +28,18 @@ public class CachedShaderCompiler extends DirectShaderCompiler {
         this.shaders = new HashMap<>();
     }
 
-    @Override
-    public CompiledShader compile(ShaderCompiler.Context context, int type, ProgramDefinition.SourceType sourceType, ResourceLocation id) throws IOException, ShaderException {
-        int hash = Objects.hash(type, id);
-        if (this.shaders.containsKey(hash)) {
-            return this.shaders.get(hash);
-        }
-        CompiledShader shader = super.compile(context, type, sourceType, id);
-        this.shaders.put(hash, shader);
-        return shader;
+    private boolean shouldCache(ShaderCompiler.Context context) {
+        var def = context.definition();
+        if (def == null) return true;
+        // Don't use cache if the shader has default preprocessor definitions,
+        // in case there are multiple program definitions using the same shader
+        return def.definitionDefaults().isEmpty();
     }
 
     @Override
     public CompiledShader compile(ShaderCompiler.Context context, int type, ProgramDefinition.SourceType sourceType, String source) throws IOException, ShaderException {
+        if (!shouldCache(context)) return super.compile(context, type, sourceType, source);
+
         int hash = Objects.hash(type, source);
         if (this.shaders.containsKey(hash)) {
             return this.shaders.get(hash);
