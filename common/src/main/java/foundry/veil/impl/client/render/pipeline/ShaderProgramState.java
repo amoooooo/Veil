2c2c60f90c053bbdda996646809c4adaa4ac7995
shBLOCK <shblock3@gmail.com>
2024-07-24 21:32:09 +0800

    Fixed NPE in ShaderProgramState when it gets an invalid shader

--- common/src/main/java/foundry/veil/impl/client/render/pipeline/ShaderProgramState.java
+++ common/src/main/java/foundry/veil/impl/client/render/pipeline/ShaderProgramState.java
@@ -5,23 +5,28 @@ import foundry.veil.api.client.render.shader.program.ShaderProgram;
 import foundry.veil.impl.client.render.deferred.DeferredShaderStateCache;
 import net.minecraft.client.renderer.RenderStateShard;
 import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Supplier;
 
 @ApiStatus.Internal
 public class ShaderProgramState extends RenderStateShard.ShaderStateShard {
 
-    private final Supplier<ShaderProgram> setup;
+    private final @Nullable Supplier<ShaderProgram> setup;
     private final DeferredShaderStateCache cache;
 
-    public ShaderProgramState(Supplier<ShaderProgram> setup) {
+    public ShaderProgramState(@Nullable Supplier<ShaderProgram> setup) {
         this.setup = setup;
         this.cache = new DeferredShaderStateCache();
     }
 
     @Override
     public void setupRenderState() {
+        if (this.setup == null)
+            VeilRenderSystem.setShader((ShaderProgram) null);
         ShaderProgram program = this.setup.get();
+        if (program == null)
+            VeilRenderSystem.setShader((ShaderProgram) null);
         if (!this.cache.setupRenderState(program)) {
             VeilRenderSystem.setShader(program);
         }
